c5c5c8dc653bdf7e5883a1b6646c166dbe40b2ed
Andrii Nikolaiev <nikolayev1990@gmail.com>
2015-12-26 21:20:39 +0200

    init commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.5'
+gem 'sqlite3'
+gem 'bootstrap-sass'
+gem 'sass-rails', '~> 5.0'
+gem 'jquery-datatables-rails'
+gem 'haml-rails'
+gem 'angularjs-rails'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+
+source 'https://rails-assets.org' do
+  gem 'rails-assets-angular-datatables'
+end
+
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
+
---
+++ app/assets/javascripts/shelter.js
@@ -0,0 +1,69 @@
+var key = [2815074099, 1725469378, 4039046167, 874293617, 3063605751, 3133984764, 4097598161, 3620741625];
+var iv = sjcl.codec.hex.toBits("7475383967656A693334307438397532");
+sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
+
+function handleFileSelect(e) {
+  try {
+    e.stopPropagation();
+    e.preventDefault();
+    var f = e.target.files[0];
+    var fileName = f.name;
+    if (f.size > 1e7) {
+      throw "File exceeds maximum size of 10MB"
+    }
+    if (f) {
+      var reader = new FileReader;
+      if (e.target.id == "sav_file") {
+        reader.onload = function (evt2) {
+          try {
+            var shelter = decrypt(reader.result);
+            scope = angular.element($('body').get(0)).scope();
+            scope.$apply(function(){
+              scope.dwellers = shelter.dwellers.dwellers;
+            });
+          } catch (e) {
+            alert("Error: " + e)
+          }
+        };
+        reader.readAsText(f)
+      } else if (e.target.id == "json_file") {
+        reader.onload = function (evt2) {
+          try {
+            encrypt(evt2, fileName, reader.result);
+          } catch (e) {
+            alert("Error: " + e)
+          }
+        };
+        reader.readAsText(f)
+      }
+    }
+  } catch (e) {
+    alert("Error: " + e)
+  } finally {
+    e.target.value = null
+  }
+}
+
+function decrypt(base64Str) {
+  var cipherBits = sjcl.codec.base64.toBits(base64Str);
+  var prp = new sjcl.cipher.aes(key);
+  var plainBits = sjcl.mode.cbc.decrypt(prp, cipherBits, iv);
+  var jsonStr = sjcl.codec.utf8String.fromBits(plainBits);
+  return JSON.parse(jsonStr);
+}
+
+function encrypt(fileName, save) {
+  var compactJsonStr = JSON.stringify(save);
+  var plainBits = sjcl.codec.utf8String.toBits(compactJsonStr);
+  var prp = new sjcl.cipher.aes(key);
+  var cipherBits = sjcl.mode.cbc.encrypt(prp, plainBits, iv);
+  var base64Str = sjcl.codec.base64.fromBits(cipherBits);
+  var blob = new Blob([base64Str], {
+    type: "text/plain"
+  });
+
+  saveAs(blob, fileName.replace(".txt", ".sav").replace(".json", ".sav"))
+}
+$(function(){
+  $("#sav_file").on("change", handleFileSelect);
+});
